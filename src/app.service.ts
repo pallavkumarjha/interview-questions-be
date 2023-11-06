785ea50bc13bd2942d6a7189131b4daf886447c0
pallav-sh <139957242+pallav-sh@users.noreply.github.com>
2023-11-06 18:14:10 +0530

    chore: added vercel,json file

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
+
+const openai = new OpenAI();
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getJobDescriptionPrompt() {
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { "role": "user", "content": "React job 3 years" }
+      ],
+      model: "gpt-3.5-turbo",
+    });
+    const responseMessages = completion.choices.map(choice => choice.message.content);
+    return responseMessages;
   }
 }
